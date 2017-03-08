031acbf46e66e2207168fe8465d78fff42f5b5fd
Benjamin J. Anderson <andeb2804@gmail.com>
2017-03-08 15:44:51 -0600

    Clear syntax error

--- template/ext/app/app.c
+++ template/ext/app/app.c
@@ -6,5 +6,5 @@ VALUE <%= @underscored_name %>_version;
 void Init_<%= @name %>() {
   <%= @underscored_name %>_version = rb_str_new("1.0", 4);
   <%= @underscored_name %> = rb_define_module("<%= @underscored_name.capitalize %>");
-  rb_define_const(<%= @underscored_name %>, "VERSION", <%= @underscored_name %>_version;);
+  rb_define_const(<%= @underscored_name %>, "VERSION", <%= @underscored_name %>_version);
 }
