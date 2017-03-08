8ebfef86c960af9b56bfee94dede3df5134e97cd
Benjamin J. Anderson <andeb2804@gmail.com>
2017-03-08 15:41:53 -0600

    Add default version

--- template/ext/app/app.c
+++ template/ext/app/app.c
@@ -1,7 +1,10 @@
 #include "<%= @underscored_name %>.h"
 
 VALUE <%= @underscored_name %>;
+VALUE <%= @underscored_name %>_version;
 
 void Init_<%= @name %>() {
-  <%= @underscored_name %> = rb_define_module("<%= @underscored_name %>");
+  <%= @underscored_name %>_version = rb_str_new("1.0", 4);
+  <%= @underscored_name %> = rb_define_module("<%= @underscored_name.capitalize %>");
+  rb_define_const(<%= @underscored_name %>, "VERSION", <%= @underscored_name %>_version;);
 }
--- template/test/app_test.rb
+++ template/test/app_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 require "<%= @underscored_name %>"
 
 class <%= @constant_name %>Test < Minitest::Test
-  def test_fails
-    flunk "No implemented tests"
+  def test_that_it_has_a_version_number
+    refute_nil ::<%= @underscored_name.capitalize %>::VERSION
   end
 end
